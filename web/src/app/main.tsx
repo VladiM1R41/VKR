74e86237761a589c31e965a8cbd8f4d99d4cf9e4
VladiM1R41 <vova_layhov@mail.ru>
2026-05-31 17:27:25 +0300

    Add frontend auth flow

--- web/src/app/main.tsx
+++ web/src/app/main.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 import { router } from './router'
+import { AuthProvider } from '../shared/auth/AuthProvider'
 import '../styles/main.css'
 
 const queryClient = new QueryClient({
@@ -18,8 +19,9 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </QueryClientProvider>
   </React.StrictMode>,
 )
-
